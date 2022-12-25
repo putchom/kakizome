d53ba9eb34e4393c4837ad67b6caf3de13d5fdc6
putchom <putchom8@gmail.com>
2022-12-25 13:15:02 +0900

    Draw

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,73 @@ import p5 from "p5";
 
 const sketch = (p: p5) => {
   p.setup = () => {
-    p.createCanvas(400, 400);
+    p.createCanvas(1000, 1000);
   };
 
+  let brushSize = 20;
+  let f;
+  let spring = 0.4;
+  let friction = 0.45;
+  let x;
+  let y;
+  let oldX;
+  let oldY;
+  let oldR;
+  let v = 0.5;
+  let r = 0;
+  let vx = 0;
+  let vy = 0;
+  let splitNum = 100;
+  let diff = 10;
+
   p.draw = () => {
-    p.background(220);
-    p.ellipse(50, 50, 80, 80);
+    if (p.mouseIsPressed) {
+      if (!f) {
+        f = true;
+        x = p.mouseX;
+        y = p.mouseY;
+      }
+      vx += (p.mouseX - x) * spring;
+      vy += (p.mouseY - y) * spring;
+      vx *= friction;
+      vy *= friction;
+
+      v += p.sqrt(vx * vx + vy * vy) - v;
+      v *= 0.55;
+
+      oldR = r;
+      r = brushSize - v;
+      var num = p.random(0.1, 0.8);
+      var num2 = p.random(0.1, 0.8);
+      for (let i = 0; i < splitNum; ++i) {
+        oldX = x;
+        oldY = y;
+        x += vx / splitNum;
+        y += vy / splitNum;
+        oldR += (r - oldR) / splitNum;
+        if (oldR < 1) {
+          oldR = 1;
+        }
+        p.strokeWeight(oldR + diff);
+        p.line(x + num, y + num, oldX + num, oldY + num);
+        p.strokeWeight(oldR);
+        p.line(
+          x + diff * num2,
+          y + diff * num2,
+          oldX + diff * num2,
+          oldY + diff * num2
+        );
+        p.line(
+          x - diff * num,
+          y - diff * num,
+          oldX - diff * num,
+          oldY - diff * num
+        );
+      }
+    } else if (f) {
+      vx = vy = 0;
+      f = false;
+    }
   };
 };
 
